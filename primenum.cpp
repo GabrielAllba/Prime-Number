18d1d88b94afcb14be24b1184198e177e30682bf
Gabriel Allba <rielgasy9@gmail.com>
2021-10-02 17:34:24 +0700

    add code and logic

---
+++ primenum.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    int input;
+    int divider = input;
+    int howManyFactor = 0;
+
+    std::string primeOrNot;
+    std::string factor;
+
+    std::cout << "input some number : ";
+    std::cin >> input;
+
+    while(divider > 0){
+        if(input % divider == 0){
+            howManyFactor += 1;
+            factor += divider + "";
+            divider -= 1;
+        }
+        else{
+            divider -= 1;
+        }
+    }
+    if(howManyFactor == 2 ){
+        primeOrNot = " is a prime number";
+    }
+    else{
+        primeOrNot = " is not a prime number";
+    }
+    std::cout << "how many factor : " << howManyFactor << "\n";
+    std::cout << "factor = " << factor << "\n";
+    std::cout << input << " " << primeOrNot << "\n";
+  
+}
